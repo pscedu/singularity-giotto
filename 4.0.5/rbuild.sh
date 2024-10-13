5289e2fba7959e26301ec1e1a2dcaa4a55c2bbbf
icaoberg <icaoberg@andrew.cmu.edu>
2024-10-13 15:40:26 -0400

    Updated and reorganized files

--- 4.0.5/rbuild.sh
+++ 4.0.5/rbuild.sh
@@ -3,11 +3,12 @@
 # Copyright © 2024 Pittsburgh Supercomputing Center.
 # All Rights Reserved.
 
-IMAGE=singularity-giotto.sif
+IMAGE=singularity-giotto-4.0.5.sif
 DEFINITION=Singularity
 
 if [ -f $IMAGE ]; then
 	rm -fv $IMAGE
 fi
 
+module load SingularityCE/4.1.2
 singularity build --remote $IMAGE $DEFINITION
